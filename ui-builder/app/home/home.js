69777cbffc8946d068384ca96f4889de5beaf90c
Karim Kawambwa <karimkawambwa@icloud.com>
2015-07-11 20:23:34 -0400

    initial node webkit setup

---
+++ ui-builder/app/home/home.js
@@ -0,0 +1,18 @@
+﻿(function () {
+  'use strict';
+
+  app.controller('homeController', function ($scope) {
+
+    var gui = require('nw.gui');
+    var os = require('os'); 
+
+    $scope.description = gui.App.manifest.description;
+    $scope.settings = [];
+
+    Object.getOwnPropertyNames(os).forEach(function(val, idx, array) { 
+      if (typeof os[val] === 'function')
+        $scope.settings.push({ name: val, value: os[val]() });  
+    });
+  });
+
+})();
