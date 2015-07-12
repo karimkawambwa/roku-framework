9e7024b93a0ac90537e797a8076f7e141517fef6
Karim Kawambwa <karimkawambwa@icloud.com>
2015-07-11 20:22:44 -0400

    initilal node webkit

---
+++ simulator/app/app.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module('MyApp', ['ngRoute']);
+
+(function () {
+  'use strict'; 
+
+  app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/home', {
+        templateUrl: 'home/home.html', 
+        controller: 'homeController'
+      })
+      .when('/about', {
+        templateUrl: 'about/about.html', 
+        controller: 'aboutController'
+      })    
+      .otherwise({  redirectTo: '/home' });
+  }); 
+
+})();
